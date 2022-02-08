d329967050ed403c31e686571e33d323f686febb
rogercodeprogr <96852253+rogercodeprogr@users.noreply.github.com>
2022-02-08 11:18:52 -0300

    Add files via upload
    Exemplo com exceção verificada

---
+++ MyExcecao.java
@@ -0,0 +1,14 @@
+
+package excecao;
+
+/**
+ *
+ * @author Acer
+ */
+public class MyExcecao extends Exception{
+    
+    public String getMsg(){
+        return "O número tem que ser positivo";        
+    }
+    
+}
